598064a1554cb7f4428bbd1e14ac876b86e12b4f
andrewbastian <andrew.bastian@gmail.com>
2020-03-03 17:56:49 -0800

    Added stretch task for add step. Also fixed typo in README regarding POST endpoint (line 89)

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,51 +1,76 @@
 const db = require('../data/dbConfig')
 
+
 function find() {
-	return db('schemes')
+    return db('schemes')
 }
+
 
 function findById(id) {
-	return db('schemes')
-	.where({id})
-	.first()
+    return db('schemes')
+        .where({
+            id
+        })
+        .first()
 }
+
 
 function findSteps(id) {
-	return db('steps')
-	.where({scheme_id: id})
+    return db('steps')
+        .where({
+            scheme_id: id
+        })
 }
+
 
 function add(newScheme) {
-  return db('schemes').insert(newScheme)
-  .then(ids => {
-    return findById(ids[0]);
-  });
+    return db('schemes')
+    .insert(newScheme)
+        .then(ids => {
+            return findById(ids[0]);
+        });
 }
+
+async function addStep(step, scheme_id) {
+	const newStep = {
+		scheme_id: scheme_id,
+		step_number: step.step_number,
+		instructions: step.instructions
+	}
+	return db('steps')
+	.insert(newStep)
+	return findSteps(newStep)
+}
+
 
 //This function is working in the DB, but returns 500 on the request...
-
 function update(changes, id) {
-	db('schemes')
-	.update(changes)
-	.where({id})
-    .then(ids => {
-    return findById(ids[0]);
-  });
+    db('schemes')
+        .update(changes)
+        .where({
+            id
+        })
+        .then(ids => {
+            return findById(ids[0]);
+        });
 }
+
 
 function remove(id) {
-	return db('schemes')
-	.where({id})
-	.del()
+    return db('schemes')
+        .where({
+            id
+        })
+        .del()
 }
 
-module.exports = {
-	find,
-	findById,
-	findSteps,
-	add,
-	update,
-	remove
 
-	
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    addStep,
+    update,
+    remove
 }
